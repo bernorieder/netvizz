8f2f0ec578931d2f98f2b51a4db2171f9a0acae7
Bernhard Rieder <rieder@MyMBA-8.local>
2015-01-23 08:54:37 +0100

    start

---
+++ privacy.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+	<title>index.php</title>
+
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+	<link href="facebook.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body class="fbbody">
+
+<h1>Privacy Policy</h1><br />
+
+Netvizz does not use downloaded data for anything beyond providing the app functionality. No data other than the the data aggregated
+in the output files is accessed. Netvizz does not store any profile or other information other than the data output files generated
+for you to download. And those are deleted from the server at regular intervals without being submitted to any form of analysis or
+aggregation. No data is stored or shared with anybody.
+
+</body>
+</html>
