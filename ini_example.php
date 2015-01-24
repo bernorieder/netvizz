31a8522c29f2fbe832759989dad8fd6085e5b9f8
Bernhard Rieder <rieder@MyMBA-8.local>
2015-01-24 09:37:16 +0100

    adding $canvasurl variable

--- ini_example.php
+++ ini_example.php
@@ -3,6 +3,8 @@
 // +++++ basic auth tokens +++++
 // register the app at http://developers.facebook.com
 
+$canvasurl = "https://apps.facebook.com/netvizz/"; 				// change to your Facebook canvas URL (normally just apps.facebook.com/nameofyourapp/)
+
 $appid = "";
 $secret = "";
 
